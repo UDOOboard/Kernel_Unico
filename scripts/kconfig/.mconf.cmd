210bacb67664a1bcab84ec78b378448dd9c4a14b
UDOOboard <info@UDOO.org>
2013-10-01 12:48:22 +0200

    kernel cleaning

--- scripts/kconfig/.mconf.cmd
+++
@@ -1,1 +0,0 @@
-cmd_scripts/kconfig/mconf := gcc  -o scripts/kconfig/mconf scripts/kconfig/mconf.o scripts/kconfig/zconf.tab.o scripts/kconfig/lxdialog/checklist.o scripts/kconfig/lxdialog/util.o scripts/kconfig/lxdialog/inputbox.o scripts/kconfig/lxdialog/textbox.o scripts/kconfig/lxdialog/yesno.o scripts/kconfig/lxdialog/menubox.o  -lncurses
